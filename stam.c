d92e719efece7f98d51031f319a2e6c7b863df9a
aviad <aviadhadad@gmail.com>
2014-05-13 13:21:35 +0300

    small changes, dummy user space program

---
+++ stam.c
@@ -0,0 +1,17 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+void foo() {
+  int i;
+  for (i=1;i<9000000000;i++)
+    ;
+  printf(1,"bla bla \n");
+}
+
+int
+main(int argc, char *argv[])
+{
+    foo();
+  exit();
+}
